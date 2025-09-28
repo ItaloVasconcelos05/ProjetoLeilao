758e54522dcd38842d09576c6434958654300b36
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-09-28 19:40:20 -0300

    feat: Implement server socket

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+
+HOST = '127.0.0.1'
+PORTA = '55555'
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORTA))
+server_socket.listen()
+
+print(f"--- Servidor escutando em {HOST}:{PORTA} ---")
+print("[...] Aguardando uma conexão [...]")
+
+client_connection, client_address =  server_socket.accept()
+
+print(f"\n [+] Conexão aceita de {client_address} [+]")
+data_received = client_connection.recv(1024).decode('utf-8')
+
+print(f"[*] Mensagem recebida: {data_received} [*]")
+
+send_response = "Recebido!"
+client_connection.send(send_response.encode('utf-8'))
+client_connection.close()
+
+print(f"f[-] Conexão com {client_address} encerrada. [-]")
+
