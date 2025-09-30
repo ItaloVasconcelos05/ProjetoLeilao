26cb2cd87ca083a9c9b1af78a71170e722aa3031
ItaloVasconcelos05 <italomiguel9705@gmail.com>
2025-09-30 20:48:21 -0300

    refactor: Implement threading lock

--- server.py
+++ server.py
@@ -1,31 +1,54 @@
-from concurrent.futures import thread
 import socket
+import string
 import threading
-import os
+
+active_clients = []
+
+client_lock = threading.Lock()
 
 HOST = '127.0.0.1'
 PORTA = 55555
 
+def broadcast(message: str, connection_sender):
+    with client_lock:
+        offline_client = []
+        for client_socket in active_clients:
+            if client_socket != connection_sender:
+                try:
+                    client_socket.send(message.encode('utf-8'))
+                except:
+                    offline_client.append(client_socket)
+
+        for client in offline_client:
+            active_clients.remove(client)
+
+
 def client_management(client_connection, client_address):
     print(f"\n[NOVA CONEXÃO] {client_address} conectado")
     
+    with client_lock:
+        active_clients.append(client_connection)
+
     try:
         while True:
             data_received = client_connection.recv(1024).decode('utf-8')
             if not data_received:
                 break
             
-            print(f" \n[{client_address}]: {data_received}")
-
-            send_response = f"\n[*] Servidor recebeu: {data_received}"
-            client_connection.send(send_response.encode('utf-8'))
+            message = f"\n[{client_address}]: {data_received}\n"
+            print(message)
+            broadcast(message, client_connection)
 
     except Exception as e:
         print(f"\n[!] Erro com {client_address}: {e}")
 
     finally:
+        with client_lock:
+            active_clients.remove(client_connection)
+
         client_connection.close()
         print(f"\n[CONEXÃO ENCERRADA] {client_address}.")
+
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
